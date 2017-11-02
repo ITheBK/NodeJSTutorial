635bdf9ba6612e2ca78952a8463c6e8283111c43
Bharath Kumar <bharath.kumar@autoninja.in>
2017-11-02 14:54:45 +0530

    DEMO Modified

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,35 @@
 const request = require ('request');
+const yargs = require('yargs');
+
+const argv = yargs
+            .options({
+              a:{
+                demand :true,
+                alias :'address',
+                describe :'Address to fetch weather for',
+                string:true
+              }
+            })
+            .help()
+            .alias('help','h')
+            .argv;
+var address = argv.address;
+console.log(`Address requested: ${address}`);
 
 request({
-  url :'http://maps.googleapis.com/maps/api/geocode/json?address=shasthripark%20kundapura',
+  url :`http://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}`,
   json:true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body,undefined,2));
+  if(error){
+    console.log('Unable to connect to server');
+  }
+  else if(body.status ==='ZERO_RESULTS'){
+    console.log('Unable to find that address');
+  }
+  else if(body.status ==='OK'){
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
+
 });
