9fe4f8b315b50fe6cba61fb815b29cf661d0d743
Bharath Kumar <bharath.kumar@autoninja.in>
2017-11-07 17:04:13 +0530

    Delete added

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -16,11 +16,29 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB Server');
 
-  db.collection('Todos').find({completed:false}).toArray().then( (docs) => {
-    console.log('Todos');
+  // db.collection('Todos').find({
+  //   _id:new ObjectID('5a0071cd485c8a699b71a986')
+  // }).toArray().then( (docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs,undefined,2));
+  // }, (err) => {
+  //   console.log('Unable to fetch the todo');
+  // })
+
+  // db.collection('Todos').find().count().then( (count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch the todo',err);
+  // })
+
+  db.collection('Users').find({name:'Bharath'}).count().then( (count) => {
+    console.log(`Name with Bharath counts: ${count}`);
+    return db.collection('Users').find({name:'Bharath'}).toArray();
+  }, (err) => {
+    console.log('Error while fetching the data from Bharath\'c collections',err);
+  }).then((docs) => {
+    console.log('All the Users item of Name bharath:');
     console.log(JSON.stringify(docs,undefined,2));
-  }, () => {
-    console.log('Unable to fetch todos');
   });
 
   //db.close();
